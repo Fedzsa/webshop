4acfae9cf6c8d4a2f93b2a381642795e88a63fda
Molnár Ferenc <fezsa97@gmail.com>
2020-02-06 12:26:55 +0100

    first commit

---
+++ resources/lang/hu/auth.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Hibás email vagy jelszó!',
+    'throttle' => 'Túl sok bejelentkezési kísérlet. Kérlek próbáld újra :seconds perc múlva.',
+    'registration' => 'Regisztráció',
+    'firstname' => 'Keresztnév',
+    'lastname' => 'Vezetéknév',
+    'email' => 'Email',
+    'password' => 'Jelszó',
+    'confirm_password' => 'Jelszó megerősítése',
+    'login' => 'Bejelentkezés',
+    'remember_me' => 'Emlékezz rám',
+    'forgot_password_link' => 'Elfelejtetted a jelszavad?',
+    'logout' => 'Kijelentkezés'
+];
---
+++ resources/views/layouts/admin_sidebar.blade.php
@@ -0,0 +1,10 @@
+<nav class="sidebar">
+    <div>
+        <ul class="sidebar-menu">
+            <li>
+                <i class="fas fa-users"></i>
+                <span>Felhasználók</span>
+            </li>
+        </ul>
+    </div>
+</nav>
