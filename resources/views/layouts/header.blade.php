ab40a23d81d0cb3589d86e4af0ea4da58ec0e207
Molnár Ferenc <fezsa97@gmail.com>
2020-02-06 13:22:17 +0100

    localization fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -42,7 +42,7 @@
                                         <a class="dropdown-item" href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                             document.getElementById('logout-form').submit();">
-                                            @lang('auth.logout')
+                                            @lang('messages.logout')
                                         </a>
             
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
