44b404d5a3014ae70a1b9c81060f3575cc98a5c5
Molnár Ferenc <fezsa97@gmail.com>
2020-05-01 12:14:23 +0200

    refact

--- app/Observers/FileObserver.php
+++ app/Observers/FileObserver.php
@@ -8,28 +8,6 @@ use App\Jobs\DeleteFile;
 class FileObserver
 {
     /**
-     * Handle the file "created" event.
-     *
-     * @param  \App\Models\File  $file
-     * @return void
-     */
-    public function created(File $file)
-    {
-        //
-    }
-
-    /**
-     * Handle the file "updated" event.
-     *
-     * @param  \App\Models\File  $file
-     * @return void
-     */
-    public function updated(File $file)
-    {
-        //
-    }
-
-    /**
      * Handle the file "deleted" event.
      *
      * @param  \App\Models\File  $file
@@ -38,27 +16,5 @@ class FileObserver
     public function deleted(File $file)
     {
         DeleteFile::dispatch($file->name);
-    }
-
-    /**
-     * Handle the file "restored" event.
-     *
-     * @param  \App\Models\File  $file
-     * @return void
-     */
-    public function restored(File $file)
-    {
-        //
-    }
-
-    /**
-     * Handle the file "force deleted" event.
-     *
-     * @param  \App\Models\File  $file
-     * @return void
-     */
-    public function forceDeleted(File $file)
-    {
-        //
     }
 }
