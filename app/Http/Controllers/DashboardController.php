6f50866e71be015b25afec24465a80eda4c0e26b
Molnár Ferenc <fezsa97@gmail.com>
2020-05-03 12:45:37 +0200

    dashboard controller

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Notification\NotificationServiceInterface;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    private NotificationServiceInterface $notificationService;
+
+    public function __construct(NotificationServiceInterface $notificationService)
+    {
+        $this->notificationService = $notificationService;
+
+        $this->middleware('auth');
+    }
+
+    public function __invoke() {
+        $notifications = $this->notificationService->getUnreadNotificationsForUser();
+
+        return view('dashboard.index', compact('notifications'));
+    }
+}
