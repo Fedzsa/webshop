595b592e8a717083b151ab9f23a3d78f6e198aa8
Molnár Ferenc <fezsa97@gmail.com>
2020-05-03 12:45:17 +0200

    autoload helper file

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Services\Notification\NotificationService;
+
+function countUnreadNotificationsForAuthUser() {
+    $notificationService = new NotificationService();
+
+    return $notificationService->countUnreadNotificationsForUser();
+}
+
+function formatStringToDateTime(string $datetime, string $format = 'Y.m.d H:i:s') {
+    $date_time = new DateTime($datetime);
+
+    return $date_time->format($format);
+}
