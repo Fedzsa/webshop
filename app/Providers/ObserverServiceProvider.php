087e9bb5412b0340b80127da72dae5377f9941ff
Molnár Ferenc <fezsa97@gmail.com>
2020-05-03 12:44:56 +0200

    model observer provider

--- app/Providers/ObserverServiceProvider.php
+++ app/Providers/ObserverServiceProvider.php
@@ -3,12 +3,15 @@
 namespace App\Providers;
 
 use App\Models\File;
+use App\Models\Product;
 use App\Observers\FileObserver;
+use App\Observers\ProductObserver;
 use Illuminate\Support\ServiceProvider;
 
-class FileServiceProvider extends ServiceProvider {
+class ObserverServiceProvider extends ServiceProvider {
 
     public function boot() {
         File::observe(FileObserver::class);
+        Product::observe(ProductObserver::class);
     }
 }
