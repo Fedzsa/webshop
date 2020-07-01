0420ec7643c74df1cf98af49613f44897ebe3bc3
Ferenc Molnár <fezsa97@gmail.com>
2020-07-01 12:21:59 +0200

    add comments to the category service and supplement the methods with return value

--- app/Services/category/CategoryService.php
+++ app/Services/category/CategoryService.php
@@ -5,6 +5,7 @@ namespace App\Services\Category;
 use App\Exceptions\CategoryNotInsertedException;
 use App\Models\Category;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Collection;
 
 class CategoryService implements CategoryServiceInterface {
     private Category $category;
@@ -13,15 +14,34 @@ class CategoryService implements CategoryServiceInterface {
         $this->category = $category;
     }
 
+    /**
+     * Get paginated categories with soft delete categories.
+     * 
+     * @param string $search The name of the product to be search.
+     * @return mixin Paginated list.
+     */
     public function getPaginatedCategories($search) {
         return $this->category->search($search)->withTrashed()->paginate(10);
     }
 
-    public function store(array $attributes) {
+    /**
+     * Store new category.
+     * 
+     * @param array $attributes
+     * @return \App\Model\Category
+     */
+    public function store(array $attributes): Category {
         return $this->category->create($attributes);
     }
 
-    public function getById(int $id) {
+    /**
+     * Get a category by id.
+     * 
+     * @param int $id
+     * @return Category
+     * @throws ModelNotFoundException
+     */
+    public function getById(int $id): Category {
         $category = $this->category->find($id);
 
         if(! isset($category))
@@ -30,6 +50,13 @@ class CategoryService implements CategoryServiceInterface {
         return $category;
     }
 
+    /**
+     * Update the category.
+     * 
+     * @param \App\Model\Category $category Category which will be update.
+     * @param array $attributes Edited category attributes.
+     * @return bool Updated or not.
+     */
     public function update(Category $category, array $attributes): bool {
         return $category->update($attributes);
     }
@@ -38,21 +65,39 @@ class CategoryService implements CategoryServiceInterface {
      * Soft delete category.
      * 
      * @param \App\Model\Category $category
-     * @return bool
+     * @return bool Deleted or not.
      */
     public function destroy(Category $category): bool {
         return $category->delete();
     }
 
+    /**
+     * Restore the soft deleted category.
+     * 
+     * @param \App\Model\Category $category Category which will be restore.
+     * @return bool Restored or not.
+     */
     public function restore(Category $category): bool {
         return $category->restore();
     }
 
-    public function search($search) {
+    /**
+     * Search product by name of the product.
+     * 
+     * @param string $search The name of the product to be search.
+     * @return Collection Category list.
+     */
+    public function search($search): Collection {
         return $this->category->search($search)->get();
     }
 
-    public function all(...$columns) {
+    /**
+     * Get categories with the specified columns.
+     * 
+     * @param array $columns Selected column names.
+     * @return Collection Category list.
+     */
+    public function all(...$columns): Collection {
         return $this->category->select('id', 'name')->get();
     }
 }
--- app/Services/category/CategoryServiceInterface.php
+++ app/Services/category/CategoryServiceInterface.php
@@ -2,15 +2,16 @@
 
 namespace App\Services\Category;
 
+use Illuminate\Database\Eloquent\Collection;
 use App\Models\Category;
 
 interface CategoryServiceInterface {
     function getPaginatedCategories(string $searchedText);
-    function search($search);
-    function getById(int $category);
-    function store(array $attributes);
+    function search($search): Collection;
+    function getById(int $category): Category;
+    function store(array $attributes): Category;
     function update(Category $category, array $attributes): bool;
     function destroy(Category $category): bool;
     function restore(Category $category): bool;
-    function all();
+    function all(): Collection;
 }
