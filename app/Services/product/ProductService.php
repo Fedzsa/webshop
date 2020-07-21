fe4b19f4f33e83b97d25402e0042cdc042a6c6ff
Ferenc Molnár <fezsa97@gmail.com>
2020-07-21 17:55:43 +0200

    add comment to product service

--- app/Services/product/ProductService.php
+++ app/Services/product/ProductService.php
@@ -14,6 +14,12 @@ class ProductService implements ProductServiceInterface {
         $this->specification = $specification;
     }
 
+    /**
+     * Get paginated products.
+     * 
+     * @param string $search - The name of the product to be search.
+     * @return mixed - paginated products
+     */
     public function getPaginatedProducts($search) {
         return $this->product->search($search)
                                 ->withTrashed()
@@ -21,14 +27,33 @@ class ProductService implements ProductServiceInterface {
                                 ->paginate(10);
     }
 
+    /**
+     * Store the new product.
+     * 
+     * @param array $attributes - product attributes
+     * @return bool
+     */
     public function store(array $attributes) {
         return $this->product->create($attributes);
     }
 
+    /**
+     * Find the product by id.
+     * 
+     * @param int $id - product id
+     * @return Product
+     */
     public function getById(int $id) {
         return Product::find($id);
     }
 
+    /**
+     * Update the product.
+     * 
+     * @param Product $product - Product which will be update.
+     * @param array $attributes - Product attributes
+     * @return bool
+     */
     public function update(Product $product, array $attributes) {
         return $product->update($attributes);
     }
@@ -53,6 +78,12 @@ class ProductService implements ProductServiceInterface {
         return $product->restore();
     }
 
+    /**
+     * Find the product by id and return with specification, file and comment.
+     * 
+     * @param int $id - product id
+     * @return Product
+     */
     public function getProductById(int $id) {
         return $this->product->with([
                                     'specifications:name',
@@ -63,6 +94,13 @@ class ProductService implements ProductServiceInterface {
                                 ->find($id, ['id', 'name', 'price', 'description', 'created_at']);
     }
 
+    /**
+     * Store the product specification.
+     * 
+     * @param Product $product
+     * @param array $attributes - specification names and values
+     * @return bool
+     */
     public function storeSpecifications(Product $product, array $attributes) {
         $specification = $this->specification->find($attributes['specification'], ['id']);
 
@@ -73,6 +111,14 @@ class ProductService implements ProductServiceInterface {
         ]);
     }
 
+    /**
+     * Update the product specification.
+     * 
+     * @param Product $product
+     * @param int $specificationId
+     * @param array $attributes - the specification name and value
+     * @return bool
+     */
     public function updateSpecification(Product $product, int $specificationId, array $attributes) {
         return $product->specifications()->updateExistingPivot($specificationId, [
             'value' => $attributes['specification-value'],
@@ -93,6 +139,13 @@ class ProductService implements ProductServiceInterface {
         ]);
     }
 
+    /**
+     * Restore the soft deleted product specification.
+     * 
+     * @param Product $product
+     * @param int $specificationId
+     * @return bool
+     */
     public function restoreSpecification(Product $product, int $specificationId) {
         return $product->specifications()->updateExistingPivot($specificationId, [
             'deleted_at' => null
