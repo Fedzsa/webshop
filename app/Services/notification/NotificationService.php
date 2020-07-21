707231bdc9ef83d7b7f4bd3470eddd81e8d25aae
Ferenc Molnár <fezsa97@gmail.com>
2020-07-21 17:40:52 +0200

    add comment

--- app/Services/notification/NotificationService.php
+++ app/Services/notification/NotificationService.php
@@ -8,16 +8,32 @@ use Illuminate\Support\Facades\DB;
 
 class NotificationService implements NotificationServiceInterface {
 
+    /**
+     * Get the authenticated user's unread notifications.
+     * 
+     * @return Notification
+     */
     public function getUnreadNotificationsForUser() {
         return Auth::user()->unreadNotifications;
     }
 
+    /**
+     * Count the unread notifications.
+     * 
+     * @return int
+     */
     public function countUnreadNotificationsForUser() {
         $userId = Auth::user()->id;
 
         return DB::table('notifications')->where('notifiable_id', $userId)->whereNull('read_at')->count();
     }
 
+    /**
+     * Mark the unread notification.
+     * 
+     * @param string $id - notification id
+     * @return bool
+     */
     public function markAsRead(string $id) {
         return DB::table('notifications')->where('id', $id)->update(['read_at' => now()]);
     }
