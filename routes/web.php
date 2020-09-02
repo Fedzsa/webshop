218cf434163294682e96ef728c8a56ff00a0edcb
Ferenc Molnár <fezsa97@gmail.com>
2020-09-02 16:10:46 +0200

    organize routes

--- routes/web.php
+++ routes/web.php
@@ -12,34 +12,47 @@
 */
 Auth::routes(['verify' => true]);
 
-Route::get('/', 'HomeController@index')->name('home');
-
-Route::get('/users', 'UserController@index')->name('users')->middleware('can:viewAny,App\Models\User');
-
-Route::resource('categories', 'CategoryController')->except(['show']);
-Route::get('/categories/{category}/delete', 'CategoryController@delete')->name('categories.delete');
-Route::put('/categories/{category}/restore', 'CategoryController@restore')->name('categories.restore');
-
-Route::resource('specifications', 'SpecificationController')->except(['show']);
-Route::get('/specifications/{specification}/delete', 'SpecificationController@delete')->name('specifications.delete');
-Route::put('/specifications/{specification}/restore', 'SpecificationController@restore')->name('specifications.restore');
 
 Route::middleware(['auth', 'verified'])->group(function() {
+    // Users
+    Route::get('/users', 'UserController@index')->name('users')->middleware('can:viewAny,App\Models\User');
+
+    // Categories
+    Route::resource('categories', 'CategoryController')->except(['show']);
+    Route::get('/categories/{category}/delete', 'CategoryController@delete')->name('categories.delete');
+    Route::put('/categories/{category}/restore', 'CategoryController@restore')->name('categories.restore');
+
+    // Specifications
+    Route::resource('specifications', 'SpecificationController')->except(['show']);
+    Route::get('/specifications/{specification}/delete', 'SpecificationController@delete')->name('specifications.delete');
+    Route::put('/specifications/{specification}/restore', 'SpecificationController@restore')->name('specifications.restore');
+
+    // Products
     Route::resource('products', 'ProductController')->except(['show']);
     Route::put('/products/{product}/restore', 'ProductController@restore');
+    Route::resource('products.specifications', 'ProductSpecificationController')->except(['show']);
+
+    // Products specifications
+    Route::put('/products/{product}/specifications/{specification}/restore', 'ProductSpecificationController@restore')->name('products.specifications.restore');
+    Route::get('/products/{product}/images', 'ProductController@images')->name('products.images');
+    Route::post('/products/{product}/images', 'ProductController@storeImage')->name('products.images.store');
+    Route::delete('/products/{product}/images/{file}', 'ProductController@destroyImage')->name('products.images.destroy');
+
+    // Product comments
+    Route::post('/products/{product}/comments', 'CommentController@store')->name('products.comments.store');
+    Route::put('/products/{product}/comments/{comment}', 'CommentController@update')->name('products.comments.update');
+
+    // Dashboard
+    Route::get('/dashboard', 'DashboardController')->name('dashboard.index');
+
+    // Notifications
+    Route::get('/notifications/unread-notification-number', 'NotificationController@unreadNumber')->name('notifications.unread_notification_number');
+    Route::put('/notifications/{id}', 'NotificationController@update')->name('notifications.update');
 });
 
+// Home
+Route::get('/', 'HomeController@index')->name('home');
+
+// Specific product
 Route::get('/products/{product}', 'ProductController@show')->name('products.show');
 
-Route::resource('products.specifications', 'ProductSpecificationController')->except(['show']);
-Route::put('/products/{product}/specifications/{specification}/restore', 'ProductSpecificationController@restore')->name('products.specifications.restore');
-Route::get('/products/{product}/images', 'ProductController@images')->name('products.images');
-Route::post('/products/{product}/images', 'ProductController@storeImage')->name('products.images.store');
-Route::delete('/products/{product}/images/{file}', 'ProductController@destroyImage')->name('products.images.destroy');
-Route::post('/products/{product}/comments', 'CommentController@store')->name('products.comments.store');
-Route::put('/products/{product}/comments/{comment}', 'CommentController@update')->name('products.comments.update');
-
-Route::get('/dashboard', 'DashboardController')->name('dashboard.index');
-
-Route::get('/notifications/unread-notification-number', 'NotificationController@unreadNumber')->name('notifications.unread_notification_number');
-Route::put('/notifications/{id}', 'NotificationController@update')->name('notifications.update');
