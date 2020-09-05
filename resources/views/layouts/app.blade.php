afc8b81b63a10e122114581e7a70c0ec3e31ac3a
Ferenc Molnár <fezsa97@gmail.com>
2020-09-05 15:49:32 +0200

    fix bug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,9 +13,11 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    @if(Auth::user()->isAdmin())
-        <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
-    @endif
+    @auth
+        @if(Auth::user()->isAdmin())
+            <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+        @endif
+    @endauth
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
